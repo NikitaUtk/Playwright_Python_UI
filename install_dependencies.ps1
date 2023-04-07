208849e0a06c85dda6dc2997130fe36c29dbbf7f
bareboux <56490972+bareboux@users.noreply.github.com>
2023-04-07 13:23:03 +0300

    script for dependencies install

---
+++ install_dependencies.ps1
@@ -0,0 +1,21 @@
+# Verify that Java was installed correctly
+Write-Host "Verifying Java installation"
+java --version
+
+# Verify that Python was installed correctly
+Write-Host "Verifying Python installation"
+python --version
+
+# Install required python modules with pip
+Write-Host "Installing required python modules with pip"
+pip install -r requirements.txt
+
+# Install playwright browser engine
+Write-Host "Installing Playwright browser engine"
+playwright install chromium
+
+# install allure with scoop manager
+Write-Host "Installing Allure with Scoop package manager"
+#Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+irm get.scoop.sh | iex
+scoop install allure
